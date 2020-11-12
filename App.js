e04e1de2908561b191ac668a78a553648176c736
Madelene Borg <madelene.borg@cybercom.com>
2020-11-12 12:04:14 +0100

    firstime page

--- App.js
+++ App.js
@@ -1,37 +1,37 @@
-import AsyncStorage from '@react-native-community/async-storage';
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AsyncStorage from "@react-native-community/async-storage";
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 import { NativeRouter, Redirect, Route } from "react-router-native";
-import { Home, Settings, FirstTime, Card } from './routes';
+import { Home, Settings, FirstTime, Card } from "./routes";
 
 export default function App() {
-  const [redirectTo, setRedirectTo] = useState('');
+  const [redirectTo, setRedirectTo] = useState("");
 
   useEffect(() => {
     isFirstTime();
-  }, [])
-  
+  }, []);
+
   const isFirstTime = async () => {
-    setRedirectTo(await AsyncStorage.getItem('isFirstTime') === 'false' ? 'card' : 'isFirstTime');
-  }
+    // setRedirectTo(await AsyncStorage.getItem('isFirstTime') === 'false' ? 'card' : 'isFirstTime');
+    setRedirectTo("isFirstTime");
+  };
 
   return (
     <View style={styles.container}>
-    <NativeRouter>
-      <Route exact path="/">
-          {redirectTo === 'card' && <Card />}
-          {redirectTo === 'isFirstTime' && <Redirect to="/firstTime" />}
-      </Route>
-      <Route path="/home" component={Home} />
-      <Route path="/settings" component={Settings} />
-      <Route path="/firstTime" component={FirstTime} />
-      <Route path="/card" component={Card} />
-    </NativeRouter>
+      <NativeRouter>
+        <Route exact path="/">
+          {redirectTo === "card" && <Card />}
+          {redirectTo === "isFirstTime" && <Redirect to="/firstTime" />}
+        </Route>
+        <Route path="/home" component={Home} />
+        <Route path="/settings" component={Settings} />
+        <Route path="/firstTime" component={FirstTime} />
+        <Route path="/card" component={Card} />
+      </NativeRouter>
     </View>
   );
-};
-
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -39,4 +39,3 @@ const styles = StyleSheet.create({
     backgroundColor: "#120507",
   },
 });
-
--- routes/FirstTime.js
+++ routes/FirstTime.js
@@ -1,30 +1,55 @@
-import React, { useEffect, useState } from 'react';
-import { Button, StyleSheet, Text, View, Alert } from 'react-native';
-import * as Permissions from 'expo-permissions';
-import * as Notifications from 'expo-notifications';
-import DateTimePicker from '@react-native-community/datetimepicker';
-import AsyncStorage from '@react-native-community/async-storage';
-import firebase from '../config/firebase';
-import { Redirect } from 'react-router-native';
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  Alert,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import * as Permissions from "expo-permissions";
+import * as Notifications from "expo-notifications";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import AsyncStorage from "@react-native-community/async-storage";
+import firebase from "../config/firebase";
+import moment from "moment";
+import { Redirect } from "react-router-native";
+import { LinearGradient } from "expo-linear-gradient";
+import logo from "./../assets/logo.png";
 
 const FirstTime = () => {
-
   const db = firebase.firestore();
   const [redirectTo, setRedirectTo] = useState(false);
-  const [expoShareToken, setExpoShareToken] = useState('');
+  const [expoShareToken, setExpoShareToken] = useState("");
+  const [showTimePicker, setShowTimePicker] = useState(false);
+  const [mySelectedTime, setMySelectedTime] = useState("My time");
   //DatePicker
   const [time, setTime] = useState(new Date(Date.now()));
 
   const onChange = async (event, selectedTime) => {
+    const isAndroid = event.type ? true : false;
+
+    if (isAndroid) {
+      setShowTimePicker(false);
+    }
     const currentDate = selectedTime || time;
+
+    // const hours = selectedTime.getHours();
+    // const minutes = selectedTime.getMinutes();
+    // console.log(hours, ":", minutes);
+
+    // const todaysDate = new Date();
+    // const selectedTimes = `${todaysDate.getHours()}:${todaysDate.getMinutes()}`;
+    // console.log(selectedTimes, "selectedtime");
     // setShow(Platform.OS === 'ios');
-    const item = await AsyncStorage.getItem('isFirstTime');
+    const item = await AsyncStorage.getItem("isFirstTime");
     setTime(currentDate);
   };
 
   useEffect(() => {
     registerForPushNotifications();
-  }, [])
+  }, []);
 
   const registerForPushNotifications = async () => {
     const { status: existingStatus } = await Permissions.getAsync(
@@ -32,60 +57,144 @@ const FirstTime = () => {
     );
     let finalStatus = existingStatus;
 
-    if (existingStatus !== 'granted') {
-      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-      finalStatus = status;
-    }
+    // if (existingStatus !== "granted") {
+    //   const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    //   finalStatus = status;
+    // }
 
-    if (finalStatus !== 'granted') {
-      console.error('Permissions was not granted, status: ', finalStatus);
-      Alert.alert('You need to give permission in order to use the app.')
-      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-      return;
-    }
+    // if (finalStatus !== "granted") {
+    //   console.error("Permissions was not granted, status: ", finalStatus);
+    //   Alert.alert("You need to give permission in order to use the app.");
+    //   const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    //   return;
+    // }
 
     let token = await Notifications.getExpoPushTokenAsync();
     setExpoShareToken(token);
-  }
+  };
+
+  const openTimePicker = () => {
+    setShowTimePicker((prevState) => !prevState);
+  };
 
   const onClick = async () => {
+    const unixedDate = moment(time).add(1, "hour").unix();
+
     const data = {
-      time,
+      time: unixedDate,
       expoShareToken,
-      usedCards: []
-    }
+      usedCards: [],
+    };
     try {
       const response = await db.collection("users").add(data);
       const id = response.id;
       try {
-        await AsyncStorage.setItem('userId', id);
-        await AsyncStorage.setItem('isFirstTime', 'false');
-        const item = await AsyncStorage.getItem('isFirstTime');
-       setRedirectTo('card');
+        await AsyncStorage.setItem("userId", id);
+        await AsyncStorage.setItem("isFirstTime", "false");
+        const item = await AsyncStorage.getItem("isFirstTime");
+        setRedirectTo("card");
       } catch (error) {
         console.log(error);
       }
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   return (
-    <View>
-      {redirectTo === 'card' && <Redirect to="/card"/>}
-      <Text>Welcome to morgonAppen</Text>
-      <Text>Please tell us when you want us to enlighten your day</Text>
-      <DateTimePicker
-        testID="timePicker"
-        value={time}
-        mode={'time'}
-        is24Hour={true}
-        display="default"
-        onChange={onChange}
-      />
-      <Button title='Great!' onPress={onClick}/>
-    </View>
+    <>
+      <View style={styles.container}>
+        {/* {redirectTo === 'card' && <Redirect to="/card"/>} */}
+        <View>
+          <Image source={logo} style={styles.logo} />
+          <Text style={styles.header}>Welcome!</Text>
+          <Text style={styles.text}>
+            Please tell us when you want us to enlighten your day
+          </Text>
+
+          <TouchableOpacity
+            onPress={openTimePicker}
+            style={styles.setTimeContainer}
+          >
+            <Text style={styles.setTimeText}>Choose time</Text>
+
+            <Text></Text>
+
+            {showTimePicker && (
+              <DateTimePicker
+                style={styles.timePicker}
+                testID="timePicker"
+                value={time}
+                mode={"time"}
+                is24Hour={true}
+                display="spinner"
+                onChange={onChange}
+              />
+            )}
+          </TouchableOpacity>
+        </View>
+
+        <LinearGradient
+          colors={["#FF5C00", "#E4862F"]}
+          style={styles.buttonContainer}
+        >
+          <TouchableOpacity onPress={onClick}>
+            <Text style={styles.buttonText}>{mySelectedTime}!</Text>
+          </TouchableOpacity>
+        </LinearGradient>
+      </View>
+    </>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  logo: {
+    alignSelf: "center",
+    marginTop: 70,
+  },
+  container: {
+    paddingVertical: 40,
+    paddingHorizontal: 20,
+    backgroundColor: "#fff",
+    display: "flex",
+    flex: 1,
+    justifyContent: "space-between",
+  },
+  header: {
+    fontSize: 52,
+    textAlign: "center",
+    paddingTop: 40,
+    paddingBottom: 20,
+  },
+  text: {
+    fontSize: 24,
+    paddingBottom: 20,
+    textAlign: "center",
+  },
+  setTimeContainer: {
+    paddingHorizontal: 10,
+    paddingVertical: 40,
+  },
+  setTimeText: {
+    textAlign: "center",
+    fontSize: 20,
+    color: "#FF5C00",
+    fontWeight: "600",
+  },
+  timePicker: {
+    position: "relative",
+  },
+  buttonContainer: {
+    borderRadius: 10,
+    display: "flex",
+    paddingVertical: 14,
+    paddingHorizontal: 10,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 24,
+    textAlign: "center",
+  },
+});
 
 export default FirstTime;
